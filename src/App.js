b18bfefdaabb8e39a1689ce2156fa301cc11813c
50vincents <vincethai035@gmail.com>
2020-08-11 00:26:24 -0700

    structured posts and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "baddy",
+      caption: "bad",
+      imageUrl: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg"
+    }
+  ]);
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="app-header">
+        <img 
+          className="app-header-image"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h1>BUILDING NA INSTAGRAM CLONE</h1>
+
+      {
+        posts.map(post => {
+          return (
+            <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+          )
+        })
+      }
+    
+
     </div>
   );
 }
